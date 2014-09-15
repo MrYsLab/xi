cefe7efb762bde6b2479946138b87163d6d16fbf
Alan Yorinks <MisterYsLab@gmail.com>
2014-09-15 17:53:02 -0400

    Modified Script to work user names containing spaces

--- servers/xiduino/windows/xiduinoInstall.bat
+++ servers/xiduino/windows/xiduinoInstall.bat
@@ -4,14 +4,14 @@ rem XiDuino for Windows install script
 echo on
 
 echo "creating a xi directory for the user"
-mkdir %userprofile%\Documents\xi
+mkdir "%userprofile%"\Documents\xi
 
 echo "copying files to the xi directory"
-copy xiduino.bat %userprofile%\Documents\xi
-copy ..\..\xiserver\xiserver.js %userprofile%\Documents\xi
-copy xi.ico %userprofile%\Documents\xi
-copy ..\..\..\clients\scratch\projects\Xi4S_Starter_Project.sb2 %userprofile%\Documents\xi
-copy ..\..\..\documentation\Xi_Install_and_Usage_Guide.pdf %userprofile%\Documents\xi
+copy xiduino.bat "%userprofile%"\Documents\xi
+copy ..\..\xiserver\xiserver.js "%userprofile%"\Documents\xi
+copy xi.ico "%userprofile%"\Documents\xi
+copy ..\..\..\clients\scratch\projects\Xi4S_Starter_Project.sb2 "%userprofile%"\Documents\xi
+copy ..\..\..\documentation\Xi_Install_and_Usage_Guide.pdf "%userprofile%"\Documents\xi
 
 echo "cleaning npm cache"
 call npm -g cache clean
