22ed7c569567c1420ae8e76607e6f296c4aafcc0
Alan Yorinks <MisterYsLab@gmail.com>
2014-09-16 13:25:07 -0400

    Changed install directory for node modules

--- servers/xiduino/windows/xiduinoInstall.bat
+++ servers/xiduino/windows/xiduinoInstall.bat
@@ -13,25 +13,11 @@ copy xi.ico "%userprofile%"\Documents\xi
 copy ..\..\..\clients\scratch\projects\Xi4S_Starter_Project.sb2 "%userprofile%"\Documents\xi
 copy ..\..\..\documentation\Xi_Install_and_Usage_Guide.pdf "%userprofile%"\Documents\xi
 
-echo "cleaning npm cache"
-call npm -g cache clean
-
-echo "loading node-gyp"
-
-call npm install -g node-gyp
-
-echo "loading johnny-five"
-
-call npm install -g johnny-five
-
-echo "loading websocket"
-
-call npm install -g websocket
-
-echo "loading open"
-
-call npm install -g open
-
+echo "installing node.js modules"
+call npm install --prefix "%userprofile%"\Documents\xi node-gyp
+call npm install --prefix "%userprofile%"\Documents\xi johnny-five
+call npm install --prefix "%userprofile%"\Documents\xi websocket
+call npm install --prefix "%userprofile%"\Documents\xi open
 echo "!!!! Install Complete !!!"
 echo
 
